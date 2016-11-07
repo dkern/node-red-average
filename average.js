cb42a3060e368be9b526c11eef997695c415d2c6
Eisbehr <git@eisbehr.de>
2016-11-07 13:29:12 +0100

    fixed indent

--- average.js
+++ average.js
@@ -8,25 +8,25 @@ module.exports = function(RED) {
         this.topics = {};
 
         this.on("input", function(msg) {
-        	if( msg.hasOwnProperty("payload") ) {
-				var input = Number(msg.payload);
+            if( msg.hasOwnProperty("payload") ) {
+                var input = Number(msg.payload);
 
-				if( !isNaN(input) && isFinite(input) ) {
-					node.topics[msg.topic.toString()] = input;
+                if( !isNaN(input) && isFinite(input) ) {
+                    node.topics[msg.topic.toString()] = input;
 
-	                var amount = 0;
-	                var sum = Object.keys(node.topics).reduce(function(a, b) {
-	                    ++amount;
-	                    return a + node.topics[b];
-	                }, 0);
+                    var amount = 0;
+                    var sum = Object.keys(node.topics).reduce(function(a, b) {
+                        ++amount;
+                        return a + node.topics[b];
+                    }, 0);
 
-	                msg.payload = sum / amount;
-	                node.send(msg);
-				}
-				else {
-					node.log("Not a number: " + msg.payload);
-				}
-			}
+                    msg.payload = sum / amount;
+                    node.send(msg);
+                }
+                else {
+                    node.log("Not a number: " + msg.payload);
+                }
+            }
         });
     }
 
